1f55dc1c9bb26bb1e886e058eee43667d8d4c5d5
rey <reygutierrez11@yahoo.com>
2022-10-01 02:21:50 -0500

    Add nav bar with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get "/nome", to: "pages#nome"
+  get "/home", to: "pages#home"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
   #get "/suggestions", to: "suggestions#index"
   #get "/suggestions/:id", to: "suggestions#show"
 
-  resources :posts, :suggestions ,:screenings
+  resources :posts, :suggestions ,:screenings, :home
 end
