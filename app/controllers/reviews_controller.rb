c62450d8edb813db01867ab49c09d55820cb4778
rey <reygutierrez11@yahoo.com>
2022-10-04 21:57:09 -0500

    Add review create form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,27 @@
 class ReviewsController < ApplicationController
   def index
   end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+  
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to @review
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:title, :author ,:body)
+    end
 end
