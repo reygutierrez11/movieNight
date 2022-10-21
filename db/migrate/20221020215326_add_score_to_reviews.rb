e6d11c30b071876dc814a8a83d8b9c42f963bc4c
rey <reygutierrez11@yahoo.com>
2022-10-20 19:34:11 -0500

    Add score to movie form and show

---
+++ db/migrate/20221020215326_add_score_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddScoreToReviews < ActiveRecord::Migration[7.0]
+  def change
+    add_column :reviews, :score, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_052043) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_215326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_052043) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score"
   end
 
   create_table "screenings", force: :cascade do |t|
