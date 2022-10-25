5bea6b8137d7d50da8d0a3f5ca013f00ec2cb8ee
rey <reygutierrez11@yahoo.com>
2022-10-25 00:38:19 -0500

    Nav change

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,7 @@ Rails.application.routes.draw do
   get "/draw", to: "pages#draw"
   get "/sketch", to: "pages#sketch"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  #get "/suggestions", to: "suggestions#index"
-  #get "/suggestions/:id", to: "suggestions#show"
 
   resources :posts, :suggestions ,:screenings, :reviews
 end
