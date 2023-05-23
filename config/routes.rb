d80525d6eb101fa1bbbdedc81a1308ce79e85e86
rey <reygutierrez11@yahoo.com>
2023-05-22 20:48:19 -0500

    Deploy the thing again

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :movies
   root 'pages#home'
   get "/nome", to: "pages#nome"
   get "/home", to: "pages#home"
   get "/fome", to: "pages#fome"
   get "/draw", to: "pages#draw"
   get "/sketch", to: "pages#sketch"
+  get "/complete", to: "pages#complete"
   devise_for :users
 
 
---
+++ db/migrate/20221110171348_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :summary
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
