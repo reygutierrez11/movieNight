0e0c35d2341e55151cf90c5e87799f95f2e1a4c3
rey <reygutierrez11@yahoo.com>
2023-08-10 16:57:48 -0500

    add maximum character count for reviews, and change review form text color to be legible with current background

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: {maximum: 100}
   validates :author, presence: true
   validates :score, presence: true
 end
