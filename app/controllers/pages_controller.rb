f6a0d36134df44c6f0e148ba4520f145a3b81cb5
rey <reygutierrez11@yahoo.com>
2023-09-23 11:48:20 -0500

    Alter home theme phase one

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
     @posts = user_signed_in? && current_user.moderator? ? Post.all.sorted : Post.published.sorted
+    @screenings = Screening.where(
+      start_time: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week)
   end
 
   def nome
